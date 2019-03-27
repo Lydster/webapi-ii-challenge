10481e16a4b54ef3ff67e6df82c2b29f88c93e24
lydiathornton <lydia.d.thornton@gmail.com>
2019-03-26 17:37:20 -0700

    almost MVP orking on update

--- data/post-router.js
+++ data/post-router.js
@@ -38,4 +38,63 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//POST
+
+router.post("/", async (req, res) => {
+  try {
+    const title = await req.body.title;
+    const contents = await req.body.contents;
+    if (title && contents) {
+      res.status(201).json(req.body);
+    } else {
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "There was an error while saving the post to the database"
+    });
+  }
+});
+
+//DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    console.log(post);
+    if (post[0]) {
+      Posts.remove(req.params.id);
+      res.status(200).json({ message: "deleted" });
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "The post could not be removed" });
+  }
+});
+
+//PUT
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    const title = await req.body.title;
+    const contents = await req.body.contents;
+    if (title && contents) {
+      Posts.update(req.body.id, post);
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "The post information could not be modified." });
+  }
+});
+
 module.exports = router;
